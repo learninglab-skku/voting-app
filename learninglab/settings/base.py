b51daf50eca6bbde24728b902005da48d8fbf2f7
Jun <jpark25@gmail.com>
2019-10-12 20:10:18 +0900

    postgres applied in settings/base.py

--- learninglab/settings/base.py
+++ learninglab/settings/base.py
@@ -107,20 +107,20 @@ WSGI_APPLICATION = 'learninglab.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+# print(get_secret("DB_PASSWORD"))
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'learninglab',
-    #     'USER': 'learninglab_user',
-    #     'PASSWORD': get_secret("DB_PASSWORD"),
-    #     'HOST': 'localhost',
-    #     # 'PORT': '',
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'learninglab',
+        'USER': 'learninglab_user',
+        'PASSWORD': get_secret("DB_PASSWORD"),
+        'HOST': 'localhost',
+        # 'PORT': '',
     },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     # 'em1_2019': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3_em1_2019'),
